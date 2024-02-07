078535f37e6f3766bf3a51b0a0b8e18eff097e8b
serhiyserhiychuk <141840178+serhiyserhiychuk@users.noreply.github.com>
2024-02-07 17:36:03 -0500

    Task 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import userData from "./userData.json";
+import friends from "./friends.json";
+import transactions from "./transactions.json";
 import Profile from "./components/Profile";
+import FriendList from "./components/Friendlist";
+import TransactionHistory from "./components/TransactionHistory";
 
 const App = () => {
   return (
@@ -11,6 +15,8 @@ const App = () => {
         image={userData.avatar}
         stats={userData.stats}
       />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
--- src/components/FriendList.jsx
+++ src/components/FriendList.jsx
@@ -1,9 +1,23 @@
+import FriendListItem from "./FriendListItem";
+
 const FriendList = (friends) => {
-  <ul>
-    <li>
-      <FriendListItem />
-    </li>
-  </ul>;
+  return (
+    <>
+      <ul>
+        {friends.friends.map((friend) => {
+          return (
+            <li key={friend.id}>
+              <FriendListItem
+                avatar={friend.avatar}
+                name={friend.name}
+                isOnline={friend.isOnline}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default FriendList;
